c5d4193e58b6473b30b03920f5df946a534277c6
Hamid FzM <hamidfzm@gmail.com>
2018-10-21 01:05:59 +0330

    Implement redux structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+import Reducers from './reducers';
 import Dashboard from './components/Dashboard';
 
 const theme = createMuiTheme();
 
+
+const store = createStore(Reducers,
+    compose(
+        applyMiddleware(thunk),
+    )
+);
+
 class App extends Component {
     render() {
         return (
-            <MuiThemeProvider theme={theme}>
-                <Dashboard/>
-            </MuiThemeProvider>
+            <Provider store={store}>
+                <MuiThemeProvider theme={theme}>
+                    <Dashboard/>
+                </MuiThemeProvider>
+            </Provider>
         );
     }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import Session from './Session';
+
+const rootReducer = combineReducers({
+    Session,
+});
+
+export default rootReducer;
