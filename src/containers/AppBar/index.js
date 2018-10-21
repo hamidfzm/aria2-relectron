1a769fa11682961ba7e28557d74cfb625d7e3c24
Hamid FzM <hamidfzm@gmail.com>
2018-10-22 01:25:12 +0330

    Implement AppBar container

---
+++ src/containers/AppBar/index.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+import AppBarComponent from '../../components/AppBar';
+import {openDrawer} from '../../actions/uiActions';
+
+class AppBar extends Component {
+    render() {
+        const {open, actions} = this.props;
+
+        return (
+            <AppBarComponent drawerOpen={open} handleDrawerOpen={actions.openDrawer}/>
+        );
+    }
+}
+
+AppBar.propTypes = {
+    open: PropTypes.node.isRequired,
+    actions: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+    const {UI: {drawer: {open}}} = state;
+    return {open}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+                openDrawer,
+            },
+            dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppBar);
